de79d8c9be0720a7fdb266287b86cfa24b92e52a
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-10-13 15:55:10 -0500

    add feature cart ui

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,20 +1,28 @@
-import { useReducer } from 'react';
-
+// import our actions
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-} from "./actions";
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
+} from '../utils/actions';
+
+import { useReducer } from 'react';
 
 export const reducer = (state, action) => {
     switch (action.type) {
-        // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+        // if action type value is the value of 'UPDATE_PRODUCTS', return a new state object with an updated products array
         case UPDATE_PRODUCTS:
             return {
                 ...state,
                 products: [...action.products]
             };
-        // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+
+        // if action type value is the value of 'UPDATE_CATEGORIES', return a new state object with an updated products array
         case UPDATE_CATEGORIES:
             return {
                 ...state,
@@ -27,11 +35,63 @@ export const reducer = (state, action) => {
                 currentCategory: action.currentCategory
             };
 
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            };
+
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products]
+            };
+
+        case REMOVE_FROM_CART:
+            // filter out passed in product id
+            let newState = state.cart.filter((product) => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
+
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map((product) => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                })
+            };
+
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            }
+
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: !state.cartOpen
+            }
+
+        // if it's none of these actions, do not update state at all and keep things the same
         default:
             return state;
     }
 };
 
+//  used to help initialize our global state object and then provide us with the functionality for updating that state by automatically running it through our custom reducer() function. Think of this as a more in-depth way of using the useState() Hook
 export function useProductReducer(initialState) {
     return useReducer(reducer, initialState);
 }
